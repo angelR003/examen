01ed4e6cc9e897358fa60db41b9dacd9dc242f5e
angelR003 <149979218+angelR003@users.noreply.github.com>
2024-02-27 00:46:31 -0600

    Update app.js

--- SRC/app.js
+++ SRC/app.js
@@ -1,17 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+
 dotenv.config();
 import router from "./routes/gym.routes.js";
-import path from "path";
+import morgan from "morgan";
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.set('views', path.join('./views/index.ejs', 'views'));
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 app.use("/api/gyms", router);
 export default app;
