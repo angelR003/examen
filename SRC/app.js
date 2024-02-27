d352f0bcf038fc528b89b10436b2dfafd318adb6
angelR003 <149979218+angelR003@users.noreply.github.com>
2024-02-27 00:31:31 -0600

    Update app.js

--- SRC/app.js
+++ SRC/app.js
@@ -2,10 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import router from "./routes/gym.routes.js";
+import path from "path";
 
 const app = express();
 
 app.set('view engine', 'ejs');
+
+app.set('views', path.join('./views/index.ejs', 'views'));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
